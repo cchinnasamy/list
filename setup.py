16236d909af78244138e76b7ff693a72be07ca89
schinnasamy <schinnasamy@burning-glass.com>
2018-07-14 12:24:17 +0530

    Just updated

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
-import setuptools
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
 
-with open("README.txt", "r") as fh:
-    long_description = fh.read()
+cmdclass = {}
+ext_modules = []
 
-setuptools.setup(
-    name="joblist",
-    version="1.0.3",
-    author="sellamani",
-    author_email="mail2.sella@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    include_package_data=True,
-    packages=setuptools.find_packages(),
-##    ext_modules=[Extension('joblist', ['joblist/trees_cython.pyd','joblist/Lookup_data_new_v2.pickle'])],
-    classifiers=(
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+if use_cython:
+    ext_modules.append(Extension("joblist.trees_cython", ['joblist/trees_cython.pyx']))
+    cmdclass.update({'build_ext': build_ext})
+else:
+    ext_modules.append(Extension("joblist.trees_cython", ['joblist/trees_cython.c']))
+
+setup(name='joblist',
+      version='0.2',
+      description="A Python implementation of pydepth",
+      long_description="A Python implementation of JobList (Data Extraction with Partial Tree Alignment)",
+      author="Sellamani",
+      author_email="mail2.sella@gmail.com",
+      install_requires=['w3lib'],
+      packages=find_packages(),
+      cmdclass=cmdclass,
+      ext_modules=ext_modules
 )
-
